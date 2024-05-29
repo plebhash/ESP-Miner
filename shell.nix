c5c0e4e522c6f69b9eb6282a361577a0101a27cd
plebhash <plebhash@proton.me>
2024-05-29 14:12:58 -0300

    add shell.nix

--- shell.nix
+++ shell.nix
@@ -1,26 +1,14 @@
-{
-  description = "A bitcoin ASIC miner for the ESP32";
-
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    esp-dev.url = "github:mirrexagon/nixpkgs-esp-dev";
+let
+  nixpkgs-esp-dev = builtins.fetchGit {
+    url = "https://github.com/mirrexagon/nixpkgs-esp-dev.git";
   };
 
-  outputs = { self, nixpkgs, esp-dev }:
-    let
-      system = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells = {
-        default = pkgs.mkShell {
-          buildInputs = [
-            esp-dev.packages.${system}.esp32-idf
-          ];
-          shellHook = ''
-            echo "ESP-IDF environment is set up. Run 'idf.py' commands here."
-            idf.py -h
-          '';
-        };
-      };
-    };
+  pkgs = import <nixpkgs> { overlays = [ (import "${nixpkgs-esp-dev}/overlay.nix") ]; };
+in
+pkgs.mkShell {
+  name = "esp-project";
+
+  buildInputs = with pkgs; [
+    esp-idf-full
+  ];
 }
